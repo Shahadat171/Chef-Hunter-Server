aaccf82eba64aad1836cd3fd12a7cbb90b5a7578
Shahadat171 <shahadathosan999@gmail.com>
2023-05-05 12:07:20 +0600

    recipe data added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000 ;
-
+app.use(cors())
 const Chef = require('./Data/Chef.json')
 
 app.get( '/', (req, res) =>{
@@ -10,6 +11,13 @@ app.get( '/', (req, res) =>{
 
 app.get('/Chef', (req, res) =>{
     res.send(Chef)
+})
+
+app.get('/Chef/:id', (req,res) =>{
+    const id = req.params.id ;
+    console.log(id)
+    const selectedChef = Chef.find(everyChef => everyChef.id == id)
+    res.send(selectedChef)
 })
 
 app.listen(port, () =>{
